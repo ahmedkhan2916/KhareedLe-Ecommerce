5106cccdf0190781f0eb742752354a58604ad61e
ahmed2916 <ahmedkhandelhi123@gmail.com>
2025-04-27 00:57:36 +0530

    commit

--- Backend/src/middlewares/Twilio.js
+++ Backend/src/middlewares/Twilio.js
@@ -1,25 +1,3 @@
-// sendSms.mjs or in a project with "type": "module" in package.json
 
-// import twilio from 'twilio';
-
-// const accountSid = 'AC3c103405912e4ed8d6f5804c91ed8d85';
-// const authToken = 'd60b651d9033ed1f1845b7191daecaad';
-
-// const client = twilio(accountSid, authToken);
-
-// client.messages
-//   .create({
-//     body: 'Congrats Tasmiya Khan your Order has been booked',
-//     from: '+16016171255',  // Your Twilio trial number
-//     to: '+919643423183'    // Your verified number
-//   })
-//   .then(message => console.log('Message sent with SID:', message.sid))
-//   .catch(error => console.error('Error sending SMS:', error));
-
-
-
-// sendWhatsapp.mjs or use "type": "module" in package.json
-
-// sendWhatsapp.mjs or use "type": "module" in package.json
 
 
